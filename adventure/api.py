c95b40d092116397db4211d38a0a0c21a11b7b0e
Jarred Stanford <stanj89@gmail.com>
2019-10-22 18:36:08 -0400

    added Maze import

--- adventure/api.py
+++ adventure/api.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from .models import *
 from rest_framework.decorators import api_view
 import json
+from .room_generation import Maze
 
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
